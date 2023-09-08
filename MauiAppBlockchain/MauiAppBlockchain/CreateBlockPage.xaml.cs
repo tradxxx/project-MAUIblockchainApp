5f9f852bbbd5bbf8fd80900616aaa108897a0e78
tradxxx <freeman.a.s@yandex.ru>
2023-09-08 22:16:28 +0300

    Maui ruinblocks Status View

--- MauiAppBlockchain/MauiAppBlockchain/CreateBlockPage.xaml.cs
+++ MauiAppBlockchain/MauiAppBlockchain/CreateBlockPage.xaml.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Json;
 using System.Text;
 using static MauiAppBlockchain.MainPage;
@@ -10,18 +11,20 @@ public partial class CreateBlockPage : ContentPage
 	public CreateBlockPage()
 	{
 		InitializeComponent();
-	}
+    }
 
 	private async void SendFromData(object sender, EventArgs e)
 	{
 		Block block = new Block
 		{
-			Data = DataEntry.Text,
+			Data = DataEntry.Text.Trim(),
 			Created = DateTime.Now,
 			Hash = string.Empty,
 			PreviousHash = string.Empty,
-			User = UserEntry.Text
-		};
+            User = UserEntry.Text.Trim()
+        };
+       
+		
 
 		HttpClient client = new HttpClient();
 		var json = JsonConvert.SerializeObject(block);
@@ -39,4 +42,6 @@ public partial class CreateBlockPage : ContentPage
 				trigger.Text = $"Ошибка\n{myError.Status}\n{myError.Last_time_block}\n{ruinblocks[0].Data}\n{ruinblocks[0].User}";
 		}
 	}
+
+    
 }
