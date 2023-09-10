7bef2868956c86a7f50e5d634608f91f7691d3cd
tradxxx <freeman.a.s@yandex.ru>
2023-09-10 18:12:24 +0300

    Init CreateForm Validator

--- MauiAppBlockchain/MauiAppBlockchain/CreateBlockPage.xaml.cs
+++ MauiAppBlockchain/MauiAppBlockchain/CreateBlockPage.xaml.cs
@@ -1,3 +1,4 @@
+using MauiAppBlockchain.Service;
 using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Json;
@@ -8,40 +9,41 @@ namespace MauiAppBlockchain;
 
 public partial class CreateBlockPage : ContentPage
 {
-	public CreateBlockPage()
-	{
-		InitializeComponent();
+    public CreateBlockPage()
+    {
+        InitializeComponent();
     }
 
-	private async void SendFromData(object sender, EventArgs e)
-	{
-		Block block = new Block
-		{
-			Data = DataEntry.Text.Trim(),
-			Created = DateTime.Now,
-			Hash = string.Empty,
-			PreviousHash = string.Empty,
-            User = UserEntry.Text.Trim()
+    private async void SendFromData(object sender, EventArgs e)
+    {
+        Block block = new Block
+        {
+            Data = DataEntry.Text,
+            Created = DateTime.Now,
+            Hash = string.Empty,
+            PreviousHash = string.Empty,
+            User = UserEntry.Text
         };
-       
-		
 
-		HttpClient client = new HttpClient();
-		var json = JsonConvert.SerializeObject(block);
-		var content = new StringContent(json, Encoding.UTF8, "application/json");
-		var response = await client.PostAsync("http://192.168.0.12:5153/api/Chain", content);
+        if (ValidatorData.ValidateCheckData(block, errorView)) 
+        {
+            HttpClient client = new HttpClient();
+            var json = JsonConvert.SerializeObject(block);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("http://192.168.0.12:5153/api/Chain", content);
 
-		if (response.IsSuccessStatusCode)
-		{
-			trigger.Text = "Успех";
-		}
-		else
-		{
-            var myError = await response.Content.ReadFromJsonAsync<BlocksData>();
-			if(myError.Blocks is IList<Block> ruinblocks)
-				trigger.Text = $"Ошибка\n{myError.Status}\n{myError.Last_time_block}\n{ruinblocks[0].Data}\n{ruinblocks[0].User}";
-		}
-	}
+            if (response.IsSuccessStatusCode)
+            {
+                sendView.Text = "Успех";
+            }
+            else
+            {
+                var myError = await response.Content.ReadFromJsonAsync<BlocksData>();
+                if (myError.Blocks is IList<Block> ruinblocks)
+                    sendView.Text = $"Ошибка\n{myError.Status}\n{myError.Last_time_block}\n{ruinblocks[0].Data}\n{ruinblocks[0].User}";
+            }
+        }
+    }
 
-    
+
 }
---
+++ MauiAppBlockchain/MauiAppBlockchain/Service/ValidatorData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.System;
+using static MauiAppBlockchain.MainPage;
+
+namespace MauiAppBlockchain.Service
+{
+    public class ValidatorData
+    {
+        public static bool ValidateCheckData(Block block,Label label)
+        {
+            var context = new ValidationContext(block);
+            var results = new List<ValidationResult>();
+            label.BackgroundColor = Colors.Black;
+
+            if (!Validator.TryValidateObject(block, context, results, true))
+            {
+                label.TextColor = Color.FromRgb(255, 0, 0); 
+                label.Text = "Не удалось создать объект Block";
+                foreach (var error in results)
+                {           
+                    label.Text += $"\n{error.ErrorMessage}";
+                }           
+                return false;
+            }
+            else
+            {
+                label.TextColor = Color.FromRgb(0, 255, 0);
+                label.Text = ($"Объект Block успешно сформирован");
+                return true;
+            }
+
+                
+        }
+    }
+}
