6e3da47acf6ec84cc8222b69aeb7469e2d0209cb
tradxxx <freeman.a.s@yandex.ru>
2023-12-25 13:34:26 +0300

    Add Picker Categories and Users

--- MauiAppBlockchain/MauiAppBlockchain/CreateBlockPage.xaml.cs
+++ MauiAppBlockchain/MauiAppBlockchain/CreateBlockPage.xaml.cs
@@ -11,10 +11,11 @@ namespace MauiAppBlockchain;
 public partial class CreateBlockPage : ContentPage
 {
     private readonly СonnectionService сonnectionService;
-    private readonly Task<IEnumerable<Category>> _categories;
-    private readonly Task<IEnumerable<User>> _users;
-    private IList<Category> categories;
-    private IList<User> users;
+
+    //private Task<IEnumerable<Category>> _categories;
+    //private Task<IEnumerable<User>> _users;
+    public IEnumerable<Category> Categories { get; set; }
+    public IEnumerable<User> Users { get; set; }
 
     public CreateBlockPage(СonnectionService сonnection)
     {
@@ -22,75 +23,103 @@ public partial class CreateBlockPage : ContentPage
         
         сonnectionService = сonnection;
 
-        _categories = сonnectionService.GetCategories();
-        _users = сonnectionService.GetUsers();
+        //_categories = сonnectionService.GetCategories();
+        //_users = сonnectionService.GetUsers();
 
-        AsyncHelperCategory();
-        AsyncHelperUser();      
+        //AsyncHelperCategory();
+        //AsyncHelperUser();
+
+        
     }
-    async void AsyncHelperCategory()
+
+    async Task InitializeAsync()
     {
-        categories = (IList<Category>)await _categories;    
+        Categories = await сonnectionService.GetCategories();
+        Users = await сonnectionService.GetUsers();
+        pickerCategories.ItemsSource = (System.Collections.IList)Categories;
+        pickerUsers.ItemsSource = (System.Collections.IList)Users;
+       
     }
-    async void AsyncHelperUser()
+
+    protected override async void OnAppearing()
     {
-        users = (IList<User>)await _users;
+        base.OnAppearing();
+
+        await InitializeAsync();
     }
+
+
+    //async void AsyncHelperCategory()
+    //{
+    //    Categories = (List<Category>)await _categories;    
+    //}
+    //async void AsyncHelperUser()
+    //{
+    //    Users = (List<User>)await _users;
+    //}
+
+
+
     private async void SendFromData(object sender, EventArgs e)
     {
         Block block = new Block
         {
             Amount = Convert.ToDecimal(AmountEntry.Text),
-            Description = DescriptionEntry.Text
+            Description = DescriptionEntry.Text,
+            CategoryId = (pickerCategories.SelectedItem as Category).Id,
+            UserId = (pickerUsers.SelectedItem as User).Id
+            
         };
 
-        
-        var category = categories.FirstOrDefault(x => x.Title == СategoryEntry.Text.Trim());
-        var user = users.FirstOrDefault(x => x.Name == UserEntry.Text.Trim());
 
-        if (category == null)
-        {
-            DisplayArtService.PrintLabelStatus(errorView, "Не найдена категория", LabelStatus.Error);
-        }
-        else if (user == null)
-        {
-            DisplayArtService.PrintLabelStatus(errorView, "Не найден пользователь", LabelStatus.Error);
-            
-        }
-        else
-        {
-            block.CategoryId = category.Id;
-            block.UserId = user.Id;
+        //var category = Categories.FirstOrDefault(x => x.Title == СategoryEntry.Text.Trim());
+        //var user = Users.FirstOrDefault(x => x.Name == UserEntry.Text.Trim());
 
-            if (ValidatorData.ValidateCheckData(block, errorView))
+        //if (category == null)
+        //{
+        //    DisplayArtService.PrintLabelStatus(errorView, "Не найдена категория", LabelStatus.Error);
+        //}
+        //else if (user == null)
+        //{
+        //    DisplayArtService.PrintLabelStatus(errorView, "Не найден пользователь", LabelStatus.Error);
+
+        //}
+        //else
+        //{
+        //    block.CategoryId = category.Id;
+        //    block.UserId = user.Id;
+
+
+
+        //}
+
+        if (ValidatorData.ValidateCheckData(block, errorView))
+        {
+            var json = JsonConvert.SerializeObject(block);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await сonnectionService.CreateBlock(content);
+
+            if (response.IsSuccessStatusCode)
             {
-                var json = JsonConvert.SerializeObject(block);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = await сonnectionService.CreateBlock(content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    sendView.Text = "Успех";
-                }
-                else
-                {
-
-                    DisplayArtService.PrintLabelStatus(errorView, "Ошибка отправки", LabelStatus.Error);
-
-                    //Вывод информации про фальшивый блок
-                    var myErrorData = await response.Content.ReadFromJsonAsync<BlocksData>();
-
-                    var ruinblocks = myErrorData.Blocks as IList<Block>;
-                    sendView.FontFamily = "OpenSansSemibold";
-                    sendView.Text = $"Ошибка данных в блоке №{ruinblocks[0].Id}:\nСтатус: {myErrorData.Status}\nВремя: {ruinblocks[0].Date}\nСумма: {ruinblocks[0].Amount}\nОписание: {ruinblocks[0].Date}\nКатегория: {ruinblocks[0].Category.Title}\nПользователь: {ruinblocks[0].User.Name}";
-
-
-                }
+                sendView.Text = "Успех";
             }
+            else
+            {
 
+                DisplayArtService.PrintLabelStatus(errorView, "Ошибка отправки", LabelStatus.Error);
+
+                //Вывод информации про фальшивый блок
+                var myErrorData = await response.Content.ReadFromJsonAsync<BlocksData>();
+
+                var ruinblocks = myErrorData.Blocks as IList<Block>;
+                sendView.FontFamily = "OpenSansSemibold";
+                sendView.Text = $"Ошибка данных в блоке №{ruinblocks[0].Id}:\nСтатус: {myErrorData.Status}\nВремя: {ruinblocks[0].Date}\nСумма: {ruinblocks[0].Amount}\nОписание: {ruinblocks[0].Date}\nКатегория: {ruinblocks[0].Category.Title}\nПользователь: {ruinblocks[0].User.Name}";
+
+
+            }
         }
 
-       
+
     }
 
 
